485d253c145b2bcf3efde8f9255a72dae99fbf63
BaptisteCoquelet <coqueletbaptiste@gmail.com>
2026-01-28 15:44:31 +0100

    fix

--- cintafactory/workflows/migrations/0001_initial.py
+++ cintafactory/workflows/migrations/0001_initial.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('contenttypes', '0002_remove_content_type_name'),
-        ('dat', '0002_initial'),
+        ('dat', '0001_initial'),
         ('users', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
