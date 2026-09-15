054b2f71fa95dacec1dfb488eaba5daee02eddc7
Abdoullah-Mhenni <abdoullahKM@gmail.com>
2026-09-15 10:29:03 +0200

    fix(eks): drop t2.medium and set desired_size to 1 for spot nodes

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,31 +1,11 @@
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"
-
-  cluster_name    = var.cluster_name
-  cluster_version = "1.30"
-
-  cluster_endpoint_public_access = true
-
-  vpc_id                   = module.vpc.vpc_id
-  subnet_ids               = module.vpc.private_subnets
-  control_plane_subnet_ids = module.vpc.public_subnets
-
-  enable_cluster_creator_admin_permissions = true
-
-  # Optimisation des coûts (FinOps) & contournement des doublons d'alias/logs
-  create_kms_key              = false
-  create_cloudwatch_log_group = false
-  cluster_encryption_config   = {}
-
-  eks_managed_node_groups = {
+eks_managed_node_groups = {
     spot_nodes = {
       min_size     = 1
       max_size     = 3
-      desired_size = 2
+      desired_size = 1
 
-      # Multi-types d'instances pour garantir l'allocation Spot (contourne UnfulfillableCapacity)
-      instance_types = ["t3.medium", "t3a.medium", "t2.medium"]
+      # Instances modernes compatibles Spot sans conflit d'offre gratuite
+      instance_types = ["t3.medium", "t3a.medium"]
       capacity_type  = "SPOT"
 
       ami_type = "AL2023_x86_64_STANDARD"
@@ -41,9 +21,3 @@ module "eks" {
       }
     }
   }
-
-  tags = {
-    Environment = var.environment
-    ManagedBy   = "Terraform"
-  }
-}
