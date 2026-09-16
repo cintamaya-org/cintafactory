b284e626fcba526e672b2fe7dadecfa25c7bbc49
Abdoullah-Mhenni <abdoullahKM@gmail.com>
2026-09-16 10:34:16 +0200

    fix(terraform): close module "eks" block and drop stray comments
    The tags block's closing brace was mistakenly used as the module's
    closing brace, leaving the module block unclosed and terraform
    validate failing.

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -14,7 +14,6 @@ module "eks" {
 
   enable_cluster_creator_admin_permissions = true
 
-  # Optimisation des coûts (FinOps)
   create_kms_key              = false
   create_cloudwatch_log_group = false
   cluster_encryption_config   = {}
@@ -25,10 +24,6 @@ module "eks" {
       max_size     = 4
       desired_size = 2
 
-      # Quota Spot vérifié le 16/09/2026 : 32 vCPU dispos sur la famille
-      # Standard (A,C,D,H,I,M,R,T,Z) en eu-west-3, 0 utilisés.
-      # m5.large = 2 vCPU / 8 Go RAM -> largement dans le quota.
-      # t3.medium gardé en repli si m5.large manque de capacité Spot ponctuellement.
       instance_types = ["m5.large", "m5a.large", "t3.medium"]
       capacity_type  = "SPOT"
 
@@ -49,4 +44,5 @@ module "eks" {
   tags = {
     Environment = var.environment
     ManagedBy   = "Terraform"
+  }
 }
